bfc23fd40fc72e1f06698f679ebe15cfcc53fbc3
cledersonbc <cledersonbc@live.com>
2020-02-16 00:10:26 -0300

    moved Python src -> CLI

---
+++ cli/src/summaryze.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+import re
+import requests
+import sys
+from bs4 import BeautifulSoup
+from requests.exceptions import ConnectionError
+
+"""
+A script to generate summary for blogspot articles.
+This software is available under GPL license.
+Author: Autociência
+Year: 2020
+License: GNU GENERAL PUBLIC LICENSE (GPL)
+"""
+
+
+def get_titles(html_doc):
+    """
+    Extracts and returns a list of titles from an HTML page.
+    :param html_doc: is a page to extract titles
+    :return: a list of titles
+    """
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    content_page = soup.find('div', {'class': 'post-body'})
+
+    if content_page is None:
+        raise ValueError(f'Blogspot article format not detected')
+
+    titles = content_page.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+
+    if len(titles) == 0:
+        raise ValueError('No title found for this page')
+
+    return titles
+
+
+def download_page(url):
+    """
+    Downloads HTML pages for valid URLs.
+    :param url: to download an HTML content
+    :return: an HTML page as string
+    """
+
+    url_pattern = r"\b((http|https):\/\/?)[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/?))"
+
+    if not re.match(url_pattern, url):
+        raise ConnectionError(f'Invalid URL format for "{url}"')
+
+    try:
+        request = requests.get(url)
+    except Exception:
+        raise ConnectionError(f'Unavailable "{url}"')
+
+    html_doc = request.text
+    request.close()
+
+    return html_doc
+
+
+def index_summary(titles):
+    """
+    Index all titles in a summary.
+    :param titles: to create a summary
+    :return: a summary with index
+    """
+
+    index = 0
+    previous_title_level = int(titles[0].name[1])
+    inner_list = ''
+    outer_list = ''
+
+    while index < len(titles):
+        html_title = titles[index]
+        title_level = int(html_title.name[1])
+        title = html_title.text.replace('\n', '').strip()
+
+        if title_level == previous_title_level:
+            # Same level title
+            inner_list += f'<li><a href="#ANCHOR">{title}</a></li>'
+            index = index + 1
+        elif title_level > previous_title_level:
+            # Subtitle (relative to previous title)
+            new_summary = index_summary(titles[index:])
+            inner_list = re.sub('</li>$', '', inner_list)
+            inner_list += f'{new_summary[0]}'
+            index = index + new_summary[1]  # jump read subtitles
+            continue
+        elif title_level < previous_title_level:
+            # Higher title (relative to previous title)
+            break
+
+        previous_title_level = title_level
+
+    outer_list = f'<ol>{inner_list}</ol>'
+
+    return [outer_list, index]
+
+
+def get_summary(titles):
+    """
+    Create an indexed summary.
+    :param titles: to create a summary
+    :return: a pretty indexed summary in HTML format
+    """
+
+    summary = f'<div class="summary-post">{index_summary(titles)[0]}</div>'
+    pretty_summary = BeautifulSoup(summary, 'lxml').body.next.prettify()
+
+    return pretty_summary
+
+
+def main(args):
+    """
+    Main function that tries download a page,
+    extract its titles and to create a summary.
+    """
+
+    try:
+        if len(args) == 1:
+            url = input('URL: ')
+        else:
+            url = args[1]
+
+        html_doc = download_page(url)
+        titles = get_titles(html_doc)
+        summary = get_summary(titles)
+        print(summary)
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ cli/tests/test_requests.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import unittest
+from src import summaryze
+from requests.exceptions import ConnectionError
+
+"""
+A script to test generated summary for blogspot articles.
+This software is available under GPL license.
+Author: Autociência
+Year: 2020
+License: GNU GENERAL PUBLIC LICENSE (GPL)
+"""
+
+
+class TestURLRequest(unittest.TestCase):
+
+    def test_invalid_url(self):
+        """
+        Tests download of an invalid web page.
+        Expected: raises exception to download invalid pages.
+        """
+
+        urls = [
+            'valid.url',
+            'invalid,url',
+            'httx://invalid.c',
+            'www.invalid',
+            'www.invalid.url.com'
+            'invalid_url'
+            'www.url.invalid.com',
+            'ftp://url.invalid.com'
+        ]
+
+        for url in urls:
+            with self.assertRaises(ConnectionError):
+                summaryze.download_page(url)
+
+    def test_valid_url(self):
+        """
+        Tests download of a valid web page.
+        Expected: returns the content of a valid page.
+        """
+
+        urls = [
+            'https://autociencia.blogspot.com/',
+            'https://autociencia.blogspot.com/2016/07/teoria-dos-conjuntos-introducao.html',
+            'http://www.google.com/'
+        ]
+
+        for url in urls:
+            page_content = summaryze.download_page(url)
+
+            self.assertIsInstance(page_content, str)
+            self.assertGreater(len(page_content), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
